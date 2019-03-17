f1aee14c7b5efd5e6c53348eab81265777609871
Rojer-GH <rojer.hamoto@gmail.com>
2019-03-17 16:33:13 +0100

    Formatted existing code

--- src/de/rojer/editablefiles/StringFile.java
+++ src/de/rojer/editablefiles/StringFile.java
@@ -8,134 +8,122 @@ import java.util.Iterator;
 
 /**
  * This class can read and load all characters from a text file
- * 
  * @author Rojer
- * 	@version 17.3.2019 15:04 UTC+1 
+ * @version 17.3.2019 15:04 UTC+1
  */
 
 public class StringFile extends EditableFile{
 
-	//Attributes
-	
-	//The text of the file
+	// Attributes
+
+	// The text of the file
 	protected String source = "";
-	
-	//Constructors
-	
+
+	// Constructors
+
 	/**
-	 * Call this method to find a file (when not found, create) and copy all its text into a usable variable
+	 * Call this method to find a file (when not found, create) and copy all its
+	 * text into a usable variable
 	 * @param the path to the folder
 	 * @param the name of the file
 	 */
-	public StringFile(String path, String fileName) {
+	public StringFile(String path, String fileName){
 		super(path, fileName);
-		try {
+		try{
 			FileReader fr = new FileReader(file);
 			BufferedReader br = new BufferedReader(fr);
 			Iterator<String> it = br.lines().iterator();
-			while(it.hasNext()) {
+			while (it.hasNext()){
 				String line = it.next();
 				source += line + "\n";
 			}
 			source += "\0";
 			br.close();
-		} catch (FileNotFoundException e) {
+		}catch(FileNotFoundException e){
 			e.printStackTrace();
-		} catch (IOException e) {
+		}catch(IOException e){
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	//EditableFile-inherited methods
-	
+
+	// EditableFile-inherited methods
+
 	/**
 	 * This method will ALWAYS return true
 	 */
 	@Override
-	public boolean getBoolean(String path) {
-		return true;
-	}
-	
+	public boolean getBoolean(String path){ return true; }
+
 	/**
 	 * This method will ALWAYS return 0.0
 	 */
 	@Override
-	public double getDouble(String path) {
-		return 0.0;
-	}
-	
+	public double getDouble(String path){ return 0.0; }
+
 	/**
 	 * This method will ALWAYS return 0.0F
 	 */
 	@Override
-	public float getFloat(String path) {
-		return 0.0F;
-	}
-	
+	public float getFloat(String path){ return 0.0F; }
+
 	/**
 	 * This method will ALWAYS return 0
 	 */
 	@Override
-	public int getInt(String path) {
-		return 0;
-	}
-	
+	public int getInt(String path){ return 0; }
+
 	/**
 	 * This method will ALWAYS return null
 	 */
 	@Override
-	public String getString(String path) {
-		return null;
-	}
-	
+	public String getString(String path){ return null; }
+
 	/**
 	 * This method won't do anything
 	 */
 	@Override
-	public void writeBoolean(String path, boolean value) {
-		
+	public void writeBoolean(String path, boolean value){
+
 	}
-	
+
 	/**
 	 * This method won't do anything
 	 */
 	@Override
-	public void writeDouble(String path, double value) {
-		
+	public void writeDouble(String path, double value){
+
 	}
-	
+
 	/**
 	 * This method won't do anything
 	 */
 	@Override
-	public void writeFloat(String path, float value) {
-		
+	public void writeFloat(String path, float value){
+
 	}
-	
+
 	/**
 	 * This method won't do anything
 	 */
 	@Override
-	public void writeInt(String path, int value) {
-		
+	public void writeInt(String path, int value){
+
 	}
-	
+
 	/**
 	 * This method won't do anything
 	 */
 	@Override
-	public void writeString(String path, String value) {
-		
+	public void writeString(String path, String value){
+
 	}
-	
-	//Getters
-	
+
+	// Getters
+
 	/**
 	 * @return the text
 	 */
-	public String getSource() {
-		return source;
-	}
+	public String getSource(){ return source; }
 
 }
--- src/de/rojer/editablefiles/exceptions/DuplicateEncodingException.java
+++ src/de/rojer/editablefiles/exceptions/DuplicateEncodingException.java
@@ -3,28 +3,26 @@ package de.rojer.editablefiles.exceptions;
 import java.util.HashMap;
 
 /**
- * This exception occurs, if an encoding list contains a minimum of two of the same characters
- * 
+ * This exception occurs, if an encoding list contains a minimum of two of the
+ * same characters
  * @author Rojer
- * 	@version 17.3.2019 15:04 UTC+1 
+ * @version 17.3.2019 15:04 UTC+1
  */
 
 public class DuplicateEncodingException extends Exception{
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private String message;
-	
-	public DuplicateEncodingException(HashMap<Character, Character> encodingList) {
+
+	public DuplicateEncodingException(HashMap<Character, Character> encodingList){
 		message = "Duplicate Encoding found!\n";
-		for(int i = 0; i < encodingList.size(); i++) {
+		for(int i = 0; i < encodingList.size(); i++){
 			message += "\'" + (char)i + "\' -> \'" + encodingList.get((char)i) + "\'\n";
 		}
 	}
-	
+
 	@Override
-	public String getMessage() {
-		return message;
-	}
+	public String getMessage(){ return message; }
 
 }
